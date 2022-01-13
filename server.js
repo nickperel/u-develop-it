ef0983fd9024841f0122089da9853dcde628450b
Kakashi <nickperel@live.com>
2022-01-13 00:03:41 -0500

    resolved issues with server and got everything caught up and working properly

--- server.js
+++ server.js
@@ -1,15 +1,35 @@
 const express = require('express');
 const PORT = process.env.PORT || 3001;
 const app = express();
+const mysql = require('mysql2');
 
 // Express middleware
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
+// Connect to database
+const db = mysql.createConnection(
+    {
+        host: 'localhost',
+        // Your MySQL Username
+        user: 'root',
+        // Your MySQL password
+        password: '',
+        database: 'election'
+    },
+    console.log('Connected to the election database.')
+);
+
+app.get('/', (req, res) => {
+    res.json({
+        message: 'Hello World'
+    });
+});
 // Defautl response for any other request (Not Found)
 app.use((req, res) => {
     res.status(404).end();
 });
+
 
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
