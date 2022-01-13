119c15d2b31343cada6fc6d98c1efdd212f3fed0
Kakashi <nickperel@live.com>
2022-01-12 20:47:02 -0500

    everything up to date and working properly before continuing

--- utils/inputCheck.js
+++ utils/inputCheck.js
@@ -1,4 +1,4 @@
-// utility to check if an object has the required properties
+ // utility to check if an object has the required properties
 // e.g., inputCheck(object, 'prop1', 'prop2', 'etc')
 
 module.exports = function(obj, ...props) {
